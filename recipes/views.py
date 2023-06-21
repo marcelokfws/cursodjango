92e9db6b24d04c268d4e07b5e8ab74bb730e9c8a
marcelokfws <marcelokfws@gmail.com>
2023-06-21 16:04:55 -0300

    criando repositorios

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 import os
 
 from django.db.models import Q
+from django.http import JsonResponse
 from django.http.response import Http404
 from django.views.generic import DetailView, ListView
 
@@ -42,6 +43,19 @@ class RecipeListViewHome(RecipeListViewBase):
 
 class RecipeListViewCategory(RecipeListViewBase):
     template_name = 'recipes/pages/category.html'
+
+
+class RecipeListViewHomeApi(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        recipes = self.get_context_data()['recipes']
+        recipes_list = recipes.object_list.values()
+
+        return JsonResponse(
+            list(recipes_list),
+            safe=False
+        )
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
